85e1137a127aabef00e1d0da264bd527231af4d0
Jonathan Bobak <jonathan.bobak@hhu.de>
2020-12-02 23:36:57 +0100

    ADD: solutions day 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import day1.DayOne;
+import day2.DayTwo;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Day One a): " + DayOne.find2020SumOfTwo("src/main/resources/dayone.txt"));
         System.out.println("Day One b): " + DayOne.find2020SumOfThree("src/main/resources/dayone.txt"));
+
+        DayTwo dayTwo = new DayTwo("src/main/resources/daytwo.txt");
+        System.out.println("Day Two a): " + dayTwo.findValidPasswordsOldRule());
+        System.out.println("Day Two b): " + dayTwo.findValidPasswordsNewRule());
     }
 }
